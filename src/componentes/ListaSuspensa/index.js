f81e3782dadf3b8e142242b8be75ecb238f5b4c2
Bruno Boaventura Martins <bruno.bm0501@hotmail.com>
2024-03-28 19:08:38 -0300

    Adicionado botão no formulário

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,8 +4,11 @@ const ListaSuspensa = (props) =>{
     return(
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select>
-            {props.itens.map(item => <option>{item}</option>)} 
+            <select required={props.obrigatorio}>
+            <option value="" disabled selected hidden>Escolha uma opção</option>
+            {props.itens.map(item => {
+        return <option key={item}>{item}</option>
+    })}
             </select>
         </div>
     )
