246672a90a8fe9ddfadab2f361bc07e90eaafa86
Bruno Boaventura Martins <bruno.bm0501@hotmail.com>
2024-04-13 12:30:40 -0300

    Adicionado botao de apagar time e limpar colaboradores

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState([])
   const [nomeTime, setNomeTime] = useState('')
-    const [corTime, setCorTime] = useState('#000000')// Se não definir uma cor inicial ele considera vazio e da erro
+  const [corTime, setCorTime] = useState('#000000')// Se não definir uma cor inicial ele considera vazio e da erro
 
 
   useEffect(() => {
@@ -84,28 +84,54 @@ function App() {
     const colaboradoresFiltrados = savedColaboradores.filter(colaborador => colaborador.id !== id)
     setColaboradores(colaboradoresFiltrados);
     localStorage.setItem('colaboradores', JSON.stringify(colaboradoresFiltrados));
-    
+
+  }
+
+  function deletarTime(id) {
+    const savedTimes = JSON.parse(localStorage.getItem('times'));
+    if (savedTimes) {
+      const timesFiltrados = savedTimes.filter(savedTime => savedTime.id !== id);
+      setTimes(timesFiltrados);
+      localStorage.setItem('times', JSON.stringify(timesFiltrados));
+    } else {
+      const timesFiltrados = times.filter(time => time.id !== id);
+      setTimes(timesFiltrados);
+      localStorage.setItem('times', JSON.stringify(timesFiltrados));
+    }
+    const timeParaExcluir = times.find(time => time.id === id).nome;
+    const colaboradoresAtualizados = colaboradores.filter(colaborador => colaborador.time !== timeParaExcluir);
+    setColaboradores(colaboradoresAtualizados);
+    localStorage.setItem('colaboradores', JSON.stringify(colaboradoresAtualizados));
+
+  }
+
+  function limparTime(id){
+    const timeParaLimpar = times.find(time => time.id === id).nome;
+    const colaboradoresAtualizados = colaboradores.filter(colaborador => colaborador.time !== timeParaLimpar);
+    setColaboradores(colaboradoresAtualizados);
+    localStorage.setItem('colaboradores', JSON.stringify(colaboradoresAtualizados));
+
   }
 
   function mudarCorDoTime(cor, id) {
     setTimes(times.map(time => {
-        if(time.id === id) {
-            time.corPrimaria = cor;
-        }
-        return time;
+      if (time.id === id) {
+        time.corPrimaria = cor;
+      }
+      return time;
     }));
-}
+  }
 
-function salvarAlteracoes(id) {
-  // Salvar os times atualizados na localStorage
-  (times.map(time => {
-    if(time.id === id) {
+  function salvarAlteracoes(id) {
+    // Salvar os times atualizados na localStorage
+    (times.map(time => {
+      if (time.id === id) {
         time.corAnterior = time.corPrimaria;
-    }
-    return time;
-}));
-  localStorage.setItem('times', JSON.stringify(times));
-}
+      }
+      return time;
+    }));
+    localStorage.setItem('times', JSON.stringify(times));
+  }
 
   useEffect(() => {
     // Carregar times salvos na localStorage ao carregar a página
@@ -115,12 +141,12 @@ function salvarAlteracoes(id) {
     }
   }, []);
 
-  function cancelarAlteracao(id){
+  function cancelarAlteracao(id) {
     const savedTimes = localStorage.getItem('times');
     if (savedTimes) {
       const timesArray = JSON.parse(savedTimes);
-      setTimes(timesArray.map(time=>{
-        if(time.id ===id){
+      setTimes(timesArray.map(time => {
+        if (time.id === id) {
           time.corPrimaria = time.corAnterior;
         }
         return time;
@@ -128,14 +154,14 @@ function salvarAlteracoes(id) {
     }
 
     setTimes(times.map(time => {
-      if(time.id === id) {
-          time.corPrimaria = time.corAnterior;
+      if (time.id === id) {
+        time.corPrimaria = time.corAnterior;
       }
       return time;
-  }));
+    }));
   }
 
-  function cadastrarTime(novoTime){
+  function cadastrarTime(novoTime) {
     const updatedTimes = [...times, { id: uuidv4(), ...novoTime, corAnterior: novoTime.corPrimaria }];
     setTimes(updatedTimes);
     // Salvar os times na localStorage
@@ -144,9 +170,9 @@ function salvarAlteracoes(id) {
     setCorTime('#000000');
   }
 
-  function verificaFavorito(id){
+  function verificaFavorito(id) {
     const updateFavoritos = colaboradores.map(colaborador => {
-      if(colaborador.id === id) colaborador.favorito = !colaborador.favorito;
+      if (colaborador.id === id) colaborador.favorito = !colaborador.favorito;
       return colaborador;
     })
     setColaboradores(updateFavoritos);
@@ -157,17 +183,19 @@ function salvarAlteracoes(id) {
   return (
     <div className="App">
       <Banner />
-      <Formulario 
-      cadastrarTime={cadastrarTime}
-      nomeTime={nomeTime}
-        setNomeTime={setNomeTime} 
-        corTime={corTime} 
+      <Formulario
+        cadastrarTime={cadastrarTime}
+        nomeTime={nomeTime}
+        setNomeTime={setNomeTime}
+        corTime={corTime}
         setCorTime={setCorTime}
-      times={times.map(time => time.nome)} 
-      colaboradorCadastrado={colaborador => colaboradorAdicionado(colaborador)} />
+        times={times.map(time => time.nome)}
+        colaboradorCadastrado={colaborador => colaboradorAdicionado(colaborador)} />
       <section className="times">
         <h1>Minha organização</h1>
         {times.map(time => <Time
+        botaoLimparTime={limparTime}
+          botaoApagarTime={deletarTime}
           id={time.id}
           mudarCor={mudarCorDoTime}
           botaoSalvar={salvarAlteracoes}
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,14 +3,18 @@ import './Time.css'
 import hexToRgba from 'hex-to-rgba';
 import { FaSave } from "react-icons/fa";
 import { AiFillCloseCircle } from 'react-icons/ai';
+import { MdDelete } from "react-icons/md";
+
 
 const Time = (props) => {
     return (
         //operador ternário para veriricar caso o time esteja vazio, não renderiza, caso tenha algum colaborador, renderiza
         (props.colaboradores.length > 0) ? <section className='time' style={{ backgroundColor: hexToRgba(props.corPrimaria, '0.4'), backgroundImage: 'url(/imagens/fundo.png)' }}>
             <div className='botoes'><input onChange={evento => props.mudarCor(evento.target.value, props.id)} value={props.corPrimaria} type='color' className='input-cor' />
-                <FaSave size={25} className='botao-salvar' onClick={evento => props.botaoSalvar(props.id)} />
-                <AiFillCloseCircle size={25} className='botao-cancelar' onClick={evento => props.botaoCancelar(props.id)} />
+                <FaSave size={25} className='botao-salvar' onClick={() => props.botaoSalvar(props.id)} />
+                <AiFillCloseCircle size={25} className='botao-cancelar' onClick={() => props.botaoCancelar(props.id)} />
+                <MdDelete size={25}  onClick={() => props.botaoApagarTime(props.id)}/>
+                    
             </div>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='colaboradores'>
@@ -18,6 +22,9 @@ const Time = (props) => {
                     return <Colaborador corDeFundo={props.corPrimaria} key={colaborador.id} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} id={colaborador.id} deletar={props.deletar} favorito={colaborador.favorito} verificaFavorito={props.favoritar} />
                 })}
             </div>
+            <button style={{ backgroundColor: props.corPrimaria }} className='limpar-time' onClick={() => props.botaoLimparTime(props.id)}>
+                Limpar time
+            </button>
         </section>
             : ''
     )
